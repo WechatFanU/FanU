4fd834c023b6f6c028ec04ab413b18d14f6e512d
cassiuschen <chzsh1995@gmail.com>
2016-12-29 23:24:45 +0800

    add InvitationInfo Template

--- api.js
+++ api.js
@@ -9,16 +9,13 @@ var Data = {
             var returnVal = {
                 special: [
                     {name: "小紫", time: "今天中午", foodType: "火锅", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "明天中午", foodType: "火锅", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "后天中午", foodType: "火锅", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"}
+                    {name: "瑄瑄", time: "明天中午", foodType: "火锅", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
+                    {name: "叶子", time: "后天中午", foodType: "火锅", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"}
                 ],
                 list: [
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
-                    {name: "小紫", time: "今天中午", foodType: "火锅", mono: "我就是想约饭你说咋办？", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"}
+                    {name: "小紫", time: "今天中午 12:30", foodType: "日本料理", restaurant: null, mono: "中午有没有人要来一起吃饭啊一个人好孤单寂寞冷。", location: "中国传媒大学", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
+                    {name: "瑄瑄", time: "今天晚上", foodType: "川菜", restaurant: {name: "四川北京办事处食堂", preview: "https://o4x2uxl34.qnssl.com/IMG_3084.JPG-preview300"}, mono: "我就是想约个饭啊。我就是想约个饭啊。我就是想约个饭啊，我就是想约个饭啊。", location: "建国路", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"},
+                    {name: "叶子", time: "明天中午", foodType: "新疆菜", restaurant:  null, mono: " 有没有人像和人家去呢……如果没人的话呢……人家就只能自己一个人去了唔……", location: "中国传媒大学", avatar: "https://ruby-china-files.b0.upaiyun.com/user/avatar/9162.jpg!lg"}
                 ]
             }
             return returnVal
